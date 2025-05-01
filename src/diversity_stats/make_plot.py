9be223e456751e09690c150d7cbae9f244389645
Emmanuel Opoku <eopoku@wm.edu>
2025-05-01 10:34:56 -0400

    Update make_plot.py

--- src/diversity_stats/make_plot.py
+++ src/diversity_stats/make_plot.py
@@ -92,3 +92,23 @@ def all_samples_barchart(data: pd.DataFrame,
     ax.legend(title=f"{level.capitalize()}", bbox_to_anchor=(1, 1))
     plt.tight_layout()
     return fig
+
+def make_barplot(data: pd.DataFrame, sample_id: str, level: str) -> None:
+    """
+    Makes a barplot of phyla and their relative abundances
+    found in a specific sample
+    Arguments:
+        data (pd.DataFrame): dataframe object containing data for the chosen sample_id./
+        It is truncated to show only top 10 phyla
+        sample_id (str): The sample_id selected. This also shows as the title of the plot
+    Returns:
+        None
+    """
+    sns.barplot(data=data, x=f"{level}_name", y="relative_abundance", hue=f"{level}_name")
+    plt.xlabel(f"{level.capitalize()} Name")
+    plt.ylabel("Relative abundance (%)")
+    plt.title(label=f"{level} Relative Abundance for sample: {sample_id}")
+    plt.xticks(rotation=90)
+    plt.show()
+    
+    return
